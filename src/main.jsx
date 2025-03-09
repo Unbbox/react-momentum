3072495a25db91ce63a6d60fc7ae62ee3cee0a69
Unbbox <ehe110894@naver.com>
2025-03-10 04:35:23 +0900

    add router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import ReactDom from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+ReactDom.createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Router>
+      <Routes>
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<App />} />
+      </Routes>
+    </Router>
+  </StrictMode>
+);
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import reactRouterPlugin from "vite-plugin-next-react-router";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), reactRouterPlugin()],
   baseUrl: "src",
 });
